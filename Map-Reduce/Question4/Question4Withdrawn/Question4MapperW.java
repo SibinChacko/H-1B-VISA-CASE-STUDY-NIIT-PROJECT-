05b9ce9625be829412e33209f646540427ab0833
Sibin Chacko <31107925+SibinChacko@users.noreply.github.com>
2017-10-24 19:21:00 +0000

    Map Reduce Program

---
+++ Map-Reduce/Question4/Question4Withdrawn/Question4MapperW.java
@@ -0,0 +1,21 @@
+
+import java.io.IOException;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class Question4MapperW extends Mapper < LongWritable, Text, Text, LongWritable > {
+    LongWritable one = new LongWritable(1);
+    public void map(LongWritable key, Text value, Context context) throws IOException,
+    InterruptedException {
+        if (key.get() > 0)
+
+        {
+            String[] token = value.toString().split("\t");
+            if (token[1].equals("WITHDRAWN")) {
+                Text answer = new Text(token[2] + "\t" + token[7]);
+                context.write(answer, one);
+            }
+        }
+    }
+}
